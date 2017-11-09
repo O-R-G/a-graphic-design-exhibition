91770094615fd9d1767bc132d451abf0d0186c43
Eric Li <ericyoungli@gmail.com>
2017-11-09 16:49:34 -0500

    updates and initial mobile:

--- public/js/main.js
+++ public/js/main.js
@@ -3,23 +3,53 @@
   $('body').click(function() {
     if (start) {
       start = false;
-      $('.read-more').toggleClass('hide-text');
+      $('#read-more').toggleClass('hide-more');
+      $('#read-more').toggleClass('show-more');
+
       $('.videos').toggleClass('hide-videos');
       $('.videos').toggleClass('show-videos');
       $('.videoWrapper').toggleClass('active');
+
+      if (player215) {
+        player215.playVideo();
+      }
+      if (player216) {
+        player216.playVideo();
+      }
+      if (player415) {
+        player415.playVideo();
+      }
     }
   })
   // Content toggle
-  $('.read-more').click(function() {
+  $('#read-more').click(function() {
     $('.videoWrapper').toggleClass('hide-video');
-    $('.full-text').toggleClass('hide-text');
-    $('.read-more').toggleClass('hide-text');
+    $('#full-text').show();
+    $('#read-more').hide();
+    $('#read-less').show();
   });
 
-  $('.read-less').click(function() {
+  $('#read-less').click(function() {
     $('.videoWrapper').toggleClass('hide-video');
-    $('.full-text').toggleClass('hide-text');
-    $('.read-more').toggleClass('hide-text');
+    $('#description').show();
+
+    $('#hello').show();
+    $('#read-more').show();
+    $('#full-text').hide();
+
+    $('#students').hide();
+    $('#colophon').hide();
+    $('#read-less').hide();
+  });
+
+  $('#students-show').click(function() {
+    $('#description').hide();
+    $('#students').show();
+  });
+
+  $('#colophon-show').click(function() {
+    $('#description').hide();
+    $('#colophon').show();
   });
 
   // Video fullscreen
@@ -35,32 +65,33 @@
         e.msRequestFullscreen();
     }
   });
-})();
 
-// Setup jump to
-function setTime(event, id) {
-  $.get('/timestamp/' + id, function(data) {
-    event.target.seekTo(data.time);
-  });
-}
+  // Setup jump to
+  function setTime(event, id) {
+    $.get('/timestamp/' + id, function(data) {
+      event.target.seekTo(data.time);
+    });
+  }
 
-function onYouTubeIframeAPIReady() {
-    var player215 = new YT.Player('vis215', {
-      events: {
-        'onReady': function(event) {
-          setTime(event, 'vis215');
-        }
-      }});
-    var player216 = new YT.Player('vis216', {
-      events: {
-        'onReady': function(event) {
-          setTime(event, 'vis216');
-        }
-      }});
-      var player415 = new YT.Player('vis415', {
+  var player215, player216, player415;
+  window.onYouTubeIframeAPIReady = function() {
+      player215 = new YT.Player('vis215', {
         events: {
           'onReady': function(event) {
-            setTime(event, 'vis415');
+            setTime(event, 'vis215');
           }
         }});
-}
+      player216 = new YT.Player('vis216', {
+        events: {
+          'onReady': function(event) {
+            setTime(event, 'vis216');
+          }
+        }});
+      player415 = new YT.Player('vis415', {
+          events: {
+            'onReady': function(event) {
+              setTime(event, 'vis415');
+            }
+          }});
+  }
+})();
