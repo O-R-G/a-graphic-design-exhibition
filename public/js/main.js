a211a09ca57dc569432f70236a566297c56e9cea
Eric Li <ericyoungli@gmail.com>
2017-11-11 19:20:52 -0500

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 (function() {
   var start = true;
-  var ready = false;
+  var allReady = false;
   $('body').click(function() {
-    if (start && ready) {
+    console.log('click');
+    if (start && allReady) {
       start = false;
-      $('#read-more').toggleClass('hide-more');
-      $('#read-more').toggleClass('show-more');
+      setTimeout(function() { $('#read-more').toggleClass('hide-text'); }, 10000);
 
       $('.videos').toggleClass('hide-videos');
       $('.videos').toggleClass('show-videos');
@@ -24,14 +24,14 @@
   })
   // Content toggle
   $('#read-more').click(function() {
-    $('.videoWrapper').toggleClass('hide-video');
+    $('.videos').hide();
     $('#full-text').show();
     $('#read-more').hide();
     $('#read-less').show();
   });
 
   $('#read-less').click(function() {
-    $('.videoWrapper').toggleClass('hide-video');
+    $('.videos').show();
     $('#description').show();
 
     $('#hello').show();
@@ -71,17 +71,41 @@
   // Setup jump to
   function setTime(event, id) {
     $.get('/timestamp/' + id, function(data) {
+      event.target.playVideo(); // double play
       event.target.seekTo(data.time);
+      console.log('ready');
+      ready[id] = true;
+      checkReady();
     });
   }
 
+  function checkReady() {
+    if (ready.vis215 && ready.vis216 && ready.vis415) {
+      console.log('all ready');
+      allReady = true;
+      $('.dots').hide();
+    }
+  }
+
+  function stateChange(state, id) {
+    if (state.data == YT.PlayerState.PLAYING) {
+      console.log('playing ' + id);
+    }
+  }
+  var ready = {
+    'vis215': false,
+    'vis216': false,
+    'vis415': false
+  };
   var player215, player216, player415;
   window.onYouTubeIframeAPIReady = function() {
-      ready = true;
       player215 = new YT.Player('vis215', {
         events: {
           'onReady': function(event) {
             setTime(event, 'vis215');
+          },
+          'onStateChange': function(state) {
+            stateChange(state, 'vis215');
           }
         }});
       player216 = new YT.Player('vis216', {
